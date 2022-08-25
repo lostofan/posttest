92422843aff83f3315908c0b5e36a211defe860a
lostofan <kolog007@gmail.com>
2022-08-25 14:05:35 +0300

    add search form

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ async function deletePost(event, id) {
     getPaginationNumbers();
     setPage(activePage);
 }
+
 const navNumbers = document.querySelector('.nav__numbers');
 let activePage;
 
@@ -77,6 +78,37 @@ function setNumberActive(activePage) {
     }
     paginationNumbers[activePage - 1].classList.add('nav__number_active')
 }
+
+const searchForm = document.getElementById('search');
+searchForm.addEventListener('input', (event) => searchStr(event))
+
+function searchStr(event) {
+    let str = event.target.value;
+    const pagination = document.querySelector('.nav__numbers');
+    pagination.hidden = true;
+    if (str === '') {
+        pagination.hidden = false;
+        return getPaginationNumbers(), setPage();
+    }
+    showSearchResult(str);
+}
+function showSearchResult(str) {
+    let postBlock = document.querySelectorAll('.post')
+    let topic = document.querySelectorAll('.post__topic');
+    let text = document.querySelectorAll('.post__text');
+
+    for (let i = 0; i < postBlock.length; i++) { 
+        let isTopicFound = topic[i].innerHTML.match(str);
+        let isTextFound = text[i].innerHTML.match(str);
+
+        postBlock[i].classList.add('hidden');
+
+        if (isTopicFound !== null || isTextFound !== null) {
+            postBlock[i].classList.remove('hidden');
+        }
+    }
+}
+
 async function loadPosts() {
     const posts = await getPosts();
     posts.map(elem => createPost(elem));
