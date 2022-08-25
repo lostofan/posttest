b195517308a00469aef563ca7c9815674cd70699
lostofan <kolog007@gmail.com>
2022-08-25 12:25:08 +0300

    add delete post button

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function createPost ({title, body, id}) {
     const button = document.createElement('button');
     button.classList.add('post__closeBtn');
     button.innerHTML = '&times';
+    button.addEventListener('click', (event) => deletePost(event, id));
 
     postItem.appendChild(article);
     postItem.appendChild(post);
@@ -29,4 +30,14 @@ function createPost ({title, body, id}) {
 async function loadPosts() {
     const posts = await getPosts();
     posts.map(elem => createPost(elem));
+}
+
+async function deletePost(event, id) {
+    event.currentTarget.disabled = true;    
+    await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+    });
+    document.querySelector(`.id_${id}`).remove();
+    getPaginationNumbers();
+    setPage(activePage);
 }
